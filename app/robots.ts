3562b5fdb8e80092828513ae084c91791baba3e9
Blake Mason <blakelmason@gmail.com>
2024-04-29 17:13:54 -0700

    remove test file

--- app/robots.ts
+++ app/robots.ts
@@ -1,4 +1,4 @@
-import { BASE_URL } from '@/test';
+import { BASE_URL } from '@/constants/pages.constants';
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
